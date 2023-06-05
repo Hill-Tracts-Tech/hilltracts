15d63e4f7e0b8873b7e91c6607bb8594f86d46bc
Rasalahmad <rasalahmadsit@gmail.com>
2023-06-05 13:45:59 +0600

    wip

--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -1,34 +1,62 @@
-/* eslint-disable no-unused-vars */
-// eslint-disable-next-line no-unused-vars
-import React from "react";
+import React, { useState } from "react";
 import { Link } from "react-router-dom";
 import navicon from "../../assets/Logo1.svg";
 
 const Navbar = () => {
+  const [isMobileMenuOpen, setIsMobileMenuOpen] = useState(false);
+
+  const toggleMobileMenu = () => {
+    setIsMobileMenuOpen(!isMobileMenuOpen);
+  };
+
+  const closeMobileMenu = () => {
+    setIsMobileMenuOpen(false);
+  };
+
   const navbarList = (
     <React.Fragment>
       <li>
-        <Link to="/" className="text-lg font-semibold  hover:text-[#1DA1F2] text-gray-500"> 
+        <Link
+          to="/"
+          className="text-lg font-semibold hover:text-[#1DA1F2] text-gray-500"
+          onClick={closeMobileMenu}
+        >
           Home
         </Link>
       </li>
       <li>
-        <Link to="/about" className="hover:text-[#1DA1F2] text-lg font-semibold  text-gray-500">
+        <Link
+          to="/about"
+          className="hover:text-[#1DA1F2] text-lg font-semibold text-gray-500"
+          onClick={closeMobileMenu}
+        >
           About
         </Link>
       </li>
       <li>
-        <Link to="/contact" className="hover:text-[#1DA1F2] text-lg font-semibold  text-gray-500">
+        <Link
+          to="/contact"
+          className="hover:text-[#1DA1F2] text-lg font-semibold text-gray-500"
+          onClick={closeMobileMenu}
+        >
           Contact
         </Link>
       </li>
       <li>
-        <Link to="/servicesDetails" className="hover:text-[#1DA1F2] text-lg font-semibold  text-gray-500">
+        <Link
+          to="/servicesDetails"
+          className="hover:text-[#1DA1F2] text-lg font-semibold text-gray-500"
+          onClick={closeMobileMenu}
+        >
           Services
         </Link>
       </li>
       <li>
-        <Link to="/" className="hover:text-[#1DA1F2] text-lg font-semibold  text-gray-500">
+        <Link
+          to="/"
+          className="hover:text-[#1DA1F2] text-lg font-semibold text-gray-500"
+          onClick={closeMobileMenu}
+        >
           Portfolio
         </Link>
       </li>
@@ -36,7 +64,7 @@ const Navbar = () => {
   );
 
   return (
-    <div className=" z-40 shadow-md navbar sticky top-0 backdrop-filter backdrop-blur-lg bg-transparent ">
+    <div className="z-40 shadow-md navbar sticky top-0 backdrop-filter backdrop-blur-lg bg-transparent">
       <div className="navbar-start hidden lg:block md:hidden lg:ml-[30px]">
         <img
           style={{ width: "80px", height: "80px" }}
@@ -49,41 +77,47 @@ const Navbar = () => {
           {navbarList}
         </ul>
       </div>
-   
-        <div className="dropdown">
-          {/* Mobile navbar */}
-          <label tabIndex={0} className="btn btn-ghost lg:hidden text-[#90cff6]">
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              className="h-5 w-5"
-              fill="none"
-              viewBox="0 0 24 24"
-              stroke="currentColor"
-            >
-              <path
-                strokeLinecap="round"
-                strokeLinejoin="round"
-                strokeWidth="2"
-                d="M4 6h16M4 12h8m-8 6h16"
-              />
-            </svg>
-          </label>
+
+      <div className="dropdown">
+        {/* Mobile navbar */}
+        <label
+          tabIndex={0}
+          className="btn btn-ghost lg:hidden text-[#90cff6]"
+          onClick={toggleMobileMenu}
+        >
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            className="h-5 w-5"
+            fill="none"
+            viewBox="0 0 24 24"
+            stroke="currentColor"
+          >
+            <path
+              strokeLinecap="round"
+              strokeLinejoin="round"
+              strokeWidth="2"
+              d="M4 6h16M4 12h8m-8 6h16"
+            />
+          </svg>
+        </label>
+        {isMobileMenuOpen && (
           <ul
             tabIndex={0}
-            className="menu menu-compact dropdown-content mt-3 p-2 shadow bg-[#b2ddf8] rounded-box w-52 "
+            className="menu menu-compact dropdown-content mt-3 p-2 shadow bg-[#b2ddf8] rounded-box w-52"
+            onClick={closeMobileMenu}
           >
             {navbarList}
           </ul>
-          {/* Mobile navbar ends */}
-        </div>
-        <div className=" block lg:hidden md:block ml-[200px] md:ml-[600px]">
-          <img
-            style={{ width: "80px", height: "80px" }}
-            src={navicon}
-            alt=""
-          ></img>
-        </div>
-      
+        )}
+        {/* Mobile navbar ends */}
+      </div>
+      <div className="block lg:hidden md:block ml-[200px] md:ml-[600px]">
+        <img
+          style={{ width: "80px", height: "80px" }}
+          src={navicon}
+          alt=""
+        ></img>
+      </div>
     </div>
   );
 };
