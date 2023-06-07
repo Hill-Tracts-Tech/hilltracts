9c768cd5a5f82d90ad3c3e5b0371556fe48d6392
UhaiMong <uhaimarma2016@gmail.com>
2023-06-07 13:25:28 +0600

    contact-form-done

--- src/Components/Loader/Loader.jsx
+++ src/Components/Loader/Loader.jsx
@@ -1,20 +1,22 @@
 import { Dna } from "react-loader-spinner";
-import './Loader.module.css';
+import "./Loader.module.css";
 const Loader = () => {
   return (
     <div className="h-[100vh] w-[100vw] bg-[#374151] z-[900000] relative">
       <div className="absolute left-[50%] top-[50%] translate-x-[-50%] translate-y-[-50%]">
-        <Dna
-          visible={true}
-          height="80"
-          width="80"
-          ariaLabel="dna-loading"
-          wrapperStyle={{}}
-                  wrapperClass="dna-wrapper"
-        />
-        <h1 className="text-center" data-text="&nbsp;Hill Tracts Tech&nbsp;">
-          &nbsp;Hill Tracts Tech&nbsp;
-        </h1>
+        <div className="flex flex-col flex-nowrap justify-center items-center">
+          <Dna
+            visible={true}
+            height="80"
+            width="80"
+            ariaLabel="dna-loading"
+            wrapperStyle={{}}
+            wrapperClass="dna-wrapper"
+          />
+          <h1 className="text-center" data-text="&nbsp;Hill Tracts Tech&nbsp;">
+            &nbsp;Hill Tracts Tech&nbsp;
+          </h1>
+        </div>
       </div>
     </div>
   );
