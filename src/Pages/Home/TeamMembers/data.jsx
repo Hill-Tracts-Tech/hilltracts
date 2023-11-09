d097864524d74dcef28139ef1b6349880232e3e2
Rasalahmad <rasalahmadsit@gmail.com>
2023-11-09 20:49:47 +0600

    wip

--- src/Pages/Home/TeamMembers/data.jsx
+++ src/Pages/Home/TeamMembers/data.jsx
@@ -22,39 +22,6 @@ export const data = [
     linkedIn: "https://www.linkedin.com/in/ahmad-rasal/",
   },
   {
-    id: 3,
-    imgURL: "https://i.ibb.co/9ssTSq7/Shahin.jpg",
-    name: "Yeasin arfat shahin",
-    designation: "CEO",
-    description:
-      "A multi-talented front-end web developer at Hill Tracts Tech Company. When not crafting exceptional websites, they enjoy exploring nature, capturing moments through photography, and empowering their community.",
-    facebook: "#",
-    github: "#",
-    linkedIn: "#",
-  },
-  {
-    id: 4,
-    imgURL: "https://i.ibb.co/gJmbQS0/uhai.jpg",
-    name: "UHai Mong",
-    designation: "Developer",
-    description:
-      "With the strong work ethic and dedication to delivering high-quality results is consistently meeting project deadlines and exceeds client expectations. Their commitment to continuous improvement and their ability to adapt to evolving technologies make them an invaluable asset to Hill Tracts Tech Company",
-    facebook: "https://facebook.com/uhaising.marma",
-    github: "https://github.com/UhaiMong",
-    linkedIn: "https://linkedin.com/uhaimong/b344",
-  },
-  {
-    id: 5,
-    imgURL: "https://i.ibb.co/WkZxYdn/dawsing.jpg",
-    name: "Dawsing",
-    designation: "Product Manager",
-    description:
-      "Keeping an eye on as continuing to make the mark in the development industry, pushing boundaries and creating digital experiences that leave a lasting impact",
-    facebook: "https://www.facebook.com/swaypai.rowock/",
-    github: "https://github.com/sway16",
-    linkedIn: "https://bd.linkedin.com/in/dawsing-marma-6b6b001b6",
-  },
-  {
     id: 6,
     imgURL: "https://i.ibb.co/s1nTTSR/talimul.jpg",
     name: "Talimul Islam",
@@ -75,60 +42,5 @@ export const data = [
     facebook: "https://www.facebook.com/imtanvirus",
     github: "#",
     linkedIn: "https://www.linkedin.com/in/tanvir-islam-890a611a5",
-  },
-  {
-    id: 8,
-    imgURL: "https://i.ibb.co/hg5YP2S/Ranghin-Mro.jpg",
-    name: "Ranghin Mro",
-    designation: "Public Relation Officer",
-    description:
-      "Get to know that, a collaborative Public Relationship officer contributing to the success of Hill Tracts Tech Company. With effective communication and problem-solving skills, they excel in delivering seamless relationship solutions",
-    facebook: "https://web.facebook.com/rang.hin.92",
-    github: "https://github.com/ranghin15",
-    linkedIn: "http://linkedin.com/in/ranghinmro",
-  },
-  {
-    id: 7,
-    imgURL: "https://i.ibb.co/tQTBLRB/umay2.jpg",
-    name: "Umay Frue Chawdhury",
-    designation: "Logistic Supporter",
-    description:
-      "Hello, a dedicated Public Relation and Logistic supporter at Hill Tracts Tech Company. Their commitment to clean, well-structured code and adherence to accessibility standards result in inclusive and engaging support experiences",
-    facebook: "#",
-    github: "#",
-    linkedIn: "#",
-  },
-  {
-    id: 8,
-    imgURL: "https://i.ibb.co/2MRC8pm/jerin.jpg",
-    name: "Ma Aye Sain",
-    designation: "Human Resource Officer",
-    description:
-      "A skilled Human Resource officer bringing creativity and a keen eye for detail to Hill Tracts Tech Company. Their proficiency in better approach to the next level.",
-    facebook: "#",
-    github: "#",
-    linkedIn: "#",
-  },
-  {
-    id: 9,
-    imgURL: "https://i.ibb.co/zJrsHw0/saksin.jpg",
-    name: "Saksin Mro",
-    designation: "Back-end Developer",
-    description:
-      " A talented back-end web developer at Hill Tracts Tech Company. With expertise in HTML, CSS, and JavaScript, creating visually stunning and user-friendly websites and functionality that leave a lasting impression",
-    facebook: "https://www.facebook.com/Engi.saksingmro?mibextid=ZbWKwL",
-    github: "#",
-    linkedIn: "https://www.linkedin.com/in/saksing-mro-b07007276",
-  },
-  {
-    id: 10,
-    imgURL: "https://i.ibb.co/5BZvb4s/sadia.jpg",
-    name: "Sadia Afrine Riya",
-    designation: "Designer",
-    description:
-      "a passionate designer at Hill Tracts Tech Company. Their constant exploration of design trends and innovative approaches keeps them at the forefront of the industry.",
-    facebook: "#",
-    github: "#",
-    linkedIn: "#",
   },
 ];
